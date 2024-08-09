3a82554c8428d0731932a8233ed1f9df2cc56855
Francisco Jiménez <guzlayer@gmail.com>
2024-08-09 10:14:41 +0200

    [main] Archivo de ejemplo de instalación y activación de paquetes

---
+++ mis_cosas/instalaciones.R
@@ -0,0 +1,13 @@
+# Instala el paquete
+install.packages("tidyverse", dependencies = TRUE)
+# Activa la biblioteca para poder utilizarla
+library(tidyverse)
+
+# Este paquete es para generar cuadrdaos mágicos
+install.packages("magic", dependencies = TRUE)
+library(magic)
+# Calcular cuadrado mágico
+magic(6)
+
+# COnsultar paquetes instalados
+installed.packages()
