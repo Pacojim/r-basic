75dcc4506dcc2d47ed741dcb736744a296ace101
Francisco Jiménez <guzlayer@gmail.com>
2024-08-14 09:49:12 +0200

    [master] Enunciado ejercicio 2 de la tarea 1

--- mis_cosas/tarea1.R
+++ mis_cosas/tarea1.R
@@ -50,3 +50,11 @@ ejercicio1 = function(segundosDesde2018) {
 }
 
 ejercicio1(250000000)
+
+# Crea una función en R que resuelva una ecuación de primer grado (de la forma Ax+B=0).
+# Es decir, los parámetros deben ser los coeficientes (en orden) y la función tiene que devolver la solución.
+# Por ejemplo, si la ecuación es 2x+4=0, la función tendría que devolver -2.
+# Una vez creada la función, utilízala para resolver las siguientes ecuaciones de primer grado:
+#  5x+3=0
+# 7x+4 = 18
+# x+1 = 1
