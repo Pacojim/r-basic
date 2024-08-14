2a6720aff0a9e30a1517e605ecb703699e9bfd71
Francisco Jiménez <guzlayer@gmail.com>
2024-08-14 12:14:01 +0200

    [master] Mi solución a la tarea 1

--- mis_cosas/tarea1.R
+++ mis_cosas/tarea1.R
@@ -1,11 +1,12 @@
+# Ejercicio 1: 
 # Si hubiéramos empezado a contar segundos a partir de las 12 campanadas que marcan el inicio de 2018,
 # ¿a qué hora de qué día de qué año llegaríamos a los 250 millones de segundos? ¡Cuidado con los años bisiestos!
 
 ejercicio1 = function(segundosDesde2018) {
  anno = 2018;
- mes = 0;
+ mes = 1;
  resueltoAno = FALSE;
- segundosSobrantes = 0;
+ segundosSobrantes = segundosDesde2018;
  diasAno = 365;
  segundosDia = 24*60*60;
  
@@ -21,26 +22,21 @@ ejercicio1 = function(segundosDesde2018) {
      }
    }
    segundosAno = diasAno*segundosDia;
-   if(segundosDesde2018%/%segundosAno >= 1) {
-     segundosDesde2018 = segundosDesde2018 - segundosAno;
+   if(segundosSobrantes>=segundosAno) {
+     segundosSobrantes = segundosSobrantes - segundosAno;
      anno = anno + 1;
    } else {
      resueltoAno = TRUE;
-     segundosSobrantes = segundosDesde2018
    }
  }
- meses = c(31*segundosDia, (if(diasAno == 365) 28 else 29)*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia);
- mesResuelto = FALSE
+ segundosMeses = c(31*segundosDia, (if(diasAno == 365) 28 else 29)*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia);
  contador = 1
- while (!mesResuelto) {
-   if(segundosSobrantes >= meses[contador]) {
-     segundosSobrantes = segundosSobrantes - meses[contador]
+ while (segundosSobrantes > segundosMeses[contador]) {
+     segundosSobrantes = segundosSobrantes - segundosMeses[contador]
      mes = mes + 1
-   } else {
-     mesResuelto = TRUE
-   }
+     contador = contador + 1
  }
- dia = segundosSobrantes %/% segundosDia
+ dia = 1 + segundosSobrantes %/% segundosDia
  segundosSobrantes = segundosSobrantes %% segundosDia
  hora = segundosSobrantes %/% (60*60)
  segundosSobrantes = segundosSobrantes %% (60*60)
@@ -51,6 +47,7 @@ ejercicio1 = function(segundosDesde2018) {
 
 ejercicio1(250000000)
 
+# Ejercicio 2: 
 # Crea una función en R que resuelva una ecuación de primer grado (de la forma Ax+B=0).
 # Es decir, los parámetros deben ser los coeficientes (en orden) y la función tiene que devolver la solución.
 # Por ejemplo, si la ecuación es 2x+4=0, la función tendría que devolver -2.
@@ -70,3 +67,14 @@ resolverEcuacion('2x+4=0', 2, 4)
 resolverEcuacion('5x+3=0', 5, 3)
 resolverEcuacion('7x+4 = 18', 7, 4, 18)
 resolverEcuacion('x+1 = 1', 1, 1, 1)
+
+# Ejercicio 3:
+# Da una expresión para calcular 3e-π con R y a continuación, da el resultado obtenido redondeado a 3 cifras decimales.
+
+round(3*exp(1) - pi, 3)
+
+# Ejercicio 4:
+# Da una expresión para calcular el módulo del número complejo (2+3i)^2/(5+8i) y, a continuación,
+# da el resultado obtenido redondeado a 3 cifras decimales.
+
+round(Mod(((2+3i)^2)/(2+8i)), 3)
