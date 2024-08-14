8de8da665777477a73a2a0195a09ff13a56c3eb0
Francisco Jiménez <guzlayer@gmail.com>
2024-08-14 09:40:47 +0200

    [master] Solución propuesta al ejercicio 1

--- mis_cosas/tarea1.R
+++ mis_cosas/tarea1.R
@@ -1,8 +1,15 @@
+# Si hubiéramos empezado a contar segundos a partir de las 12 campanadas que marcan el inicio de 2018,
+# ¿a qué hora de qué día de qué año llegaríamos a los 250 millones de segundos? ¡Cuidado con los años bisiestos!
+
 ejercicio1 = function(segundosDesde2018) {
  anno = 2018;
- resuelto = FALSE;
+ mes = 0;
+ resueltoAno = FALSE;
+ segundosSobrantes = 0;
+ diasAno = 365;
+ segundosDia = 24*60*60;
  
- while (!resuelto) {
+ while (!resueltoAno) {
    diasAno = 365;
    if(anno%%4 == 0) {
      if(anno%%100 != 0) {
@@ -13,12 +20,33 @@ ejercicio1 = function(segundosDesde2018) {
        }
      }
    }
-   segundosAno = diasAno*24*60*60;
-   if(segundosDesde2018%/%segundosAno > 1) {
+   segundosAno = diasAno*segundosDia;
+   if(segundosDesde2018%/%segundosAno >= 1) {
      segundosDesde2018 = segundosDesde2018 - segundosAno;
      anno = anno + 1;
    } else {
-     resuelto = TRUE;
+     resueltoAno = TRUE;
+     segundosSobrantes = segundosDesde2018
    }
  }
+ meses = c(31*segundosDia, (if(diasAno == 365) 28 else 29)*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia, 30*segundosDia, 31*segundosDia);
+ mesResuelto = FALSE
+ contador = 1
+ while (!mesResuelto) {
+   if(segundosSobrantes >= meses[contador]) {
+     segundosSobrantes = segundosSobrantes - meses[contador]
+     mes = mes + 1
+   } else {
+     mesResuelto = TRUE
+   }
+ }
+ dia = segundosSobrantes %/% segundosDia
+ segundosSobrantes = segundosSobrantes %% segundosDia
+ hora = segundosSobrantes %/% (60*60)
+ segundosSobrantes = segundosSobrantes %% (60*60)
+ minutos = segundosSobrantes %/% 60
+ segundosSobrantes = segundosSobrantes %% 60
+ sprintf("La fecha es %d/%d/%d %d:%d:%d", dia, mes, anno, hora, minutos, segundosSobrantes)
 }
+
+ejercicio1(250000000)
